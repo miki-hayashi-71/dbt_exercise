66c31ea7fbeeda404890437bdd6e302a05e4d6bd
miki-hayashi-71 <miki.hayashi.71+work@gmail.com>
2025-11-25 12:32:39 +0900

    テーブル名の修正とそれに付随する修正の実施

--- target/run/dbt_exercise/models/staging/stg__order_items.sql
+++ target/run/dbt_exercise/models/staging/stg__order_items.sql
@@ -1,6 +1,6 @@
 
 
-  create or replace view `data-bootcamp-477503`.`dbt_hayashi`.`stg_order_items`
+  create or replace view `data-bootcamp-477503`.`dbt_hayashi`.`stg__order_items`
   OPTIONS()
   as select * from `bigquery-public-data`.`thelook_ecommerce`.`order_items`;
 
