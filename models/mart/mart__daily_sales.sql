3cd00c1eed57eba95a2f7f476f7f67cdbb690380
miki-hayashi-71 <miki.hayashi.71+work@gmail.com>
2025-11-27 12:28:20 +0900

    mart__daily_salesのconfigを追加

--- models/mart/mart__daily_sales.sql
+++ models/mart/mart__daily_sales.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        materialized="table",
+        partition_by={"field": "date", "data_type": "date", "granularity": "day"},
+    )
+}}
+
 with cleansed_orders as (select * from {{ ref("int__cleansed_orders") }})
 
 select
