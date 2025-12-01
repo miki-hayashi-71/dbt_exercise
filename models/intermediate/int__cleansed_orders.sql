b8f48497c4b1f734ba7df72399b3fc7eb2ff00d0
miki-hayashi-71 <miki.hayashi.71+work@gmail.com>
2025-12-01 17:25:56 +0900

    int__cleansed_ordersにstg__productsの情報を追加

--- models/intermediate/int__cleansed_orders.sql
+++ models/intermediate/int__cleansed_orders.sql
@@ -15,7 +15,9 @@
 with
     orders as (select * from {{ ref("stg__orders") }}),
 
-    order_items as (select * from {{ ref("stg__order_items") }})
+    order_items as (select * from {{ ref("stg__order_items") }}),
+
+    products as (select * from {{ ref("stg__products") }})
 
 select
     orders.order_id,
@@ -23,9 +25,14 @@ select
     datetime(orders.created_at, "+9") as order_time_jst,
     order_items.product_id,
     order_items.inventory_item_id,
-    order_items.sale_price * 150 as sales_jpy
+    order_items.sale_price * 150 as sales_jpy,
+    products.category as product_category,
+    products.name as product_name,
+    products.brand as product_brand,
+    products.department as product_department
 from orders
 left join order_items on orders.order_id = order_items.order_id
+left join products on order_items.product_id = products.id
 where
     order_items.status not in ("Cancelled", "Returned")
     or orders.status not in ("Cancelled", "Returned")
