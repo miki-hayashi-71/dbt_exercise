666f5cb98fc8f4d1004a6a71e58eda91d3f847d6
miki-hayashi-71 <miki.hayashi.71+work@gmail.com>
2025-12-04 18:57:03 +0900

    集計ロジックを final CTEに移動

--- models/intermediate/int__monthly_registered_user_types.sql
+++ models/intermediate/int__monthly_registered_user_types.sql
@@ -14,17 +14,22 @@
 with
     daily_registered_user_types as (
         select * from {{ ref("int__daily_registered_user_types") }}
+    ),
+
+    final as (
+        select
+            date_trunc(date, month) as month,
+            user_id,
+            case
+                when countif(user_type = "新規") > 0
+                then "新規"
+                when countif(user_type = "復帰") > 0
+                then "復帰"
+                else "既存"
+            end as user_type
+        from daily_registered_user_types
+        group by 1, 2
     )
 
-select
-    date_trunc(date, month) as month,
-    user_id,
-    case
-        when countif(user_type = "新規") > 0
-        then "新規"
-        when countif(user_type = "復帰") > 0
-        then "復帰"
-        else "既存"
-    end as user_type
-from daily_registered_user_types
-group by 1, 2
+select *
+from final
